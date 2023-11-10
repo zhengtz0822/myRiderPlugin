62c71981ba34f6906e08b81543886a7f08b3a5ce
GitHub Action <action@github.com>
2023-11-10 10:09:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhengtz0822/myriderplugin/MyBundle.kt
+++ src/main/kotlin/com/github/zhengtz0822/myriderplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhengtz0822.myriderplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhengtz0822/myriderplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhengtz0822/myriderplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhengtz0822.myriderplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhengtz0822.myriderplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
